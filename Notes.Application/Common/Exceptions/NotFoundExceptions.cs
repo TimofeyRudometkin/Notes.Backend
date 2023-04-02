49984c71d1f78300f240f6d6b65eb876183b780c
Тимофей <timan2008@yandex.ru>
2023-04-02 23:38:58 +0400

    Добавьте файлы проекта.

---
+++ Notes.Application/Common/Exceptions/NotFoundExceptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Common.Exceptions
+{
+    public class NotFoundExceptions : Exception
+    {
+        public NotFoundExceptions(string name, object key) : base($"Entity  \"{name}\" ({key}) not found.") { }
+    }
+}
